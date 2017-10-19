50c520976e33e8cbc5c36b963ca23908750c1386
Paul Heasley <paul.heasley@pageuppeople.com>
2017-10-19 11:21:45 +1100

    Simple demo of Flakey Snowflake generator encoding / decoding using base64

---
+++ SnowflakeDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Flakey;
+
+namespace SnowflakeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = new IdGenerator(0);
+            for (var i = 0; i < 100; i++)
+            {
+                var id = generator.CreateId();
+                Console.WriteLine($"Base10: {id}, Encoded: {Encode(id)}, Decodes Correctly: {Decode(Encode(id)) == id}");
+            }
+        }
+
+        public static string Encode(long uid)
+        {
+            string encoded = Convert.ToBase64String(BitConverter.GetBytes(uid));
+            encoded = encoded
+                .Replace("/", "_")
+                .Replace("+", "-");
+            return encoded.Substring(0, 11);
+        }
+
+        public static long Decode(string encoded)
+        {
+            encoded = encoded.Replace("_", "/");
+            encoded = encoded.Replace("-", "+");
+            byte[] buffer = Convert.FromBase64String(encoded + "=");
+            return BitConverter.ToInt64(buffer, 0);
+        }
+    }
+}
